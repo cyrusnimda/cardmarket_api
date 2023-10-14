e1c9104d08b707f1eae2edde46cafa47b7fc5347
Josu <cyrusnimda@gmail.com>
2023-10-14 12:36:34 +0100

    First commit

---
+++ var/cache/dev/url_matching_routes.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * This file has been auto-generated
+ * by the Symfony Routing Component.
+ */
+
+return [
+    false, // $matchHost
+    [ // $staticRoutes
+        '/cards/random' => [[['_route' => 'get_random_cards', '_controller' => 'App\\Controller\\CardController::getRandomCards'], null, null, null, false, false, null]],
+    ],
+    [ // $regexpList
+        0 => '{^(?'
+                .'|/_error/(\\d+)(?:\\.([^/]++))?(*:35)'
+                .'|/cards/(\\d+)(*:54)'
+            .')/?$}sDu',
+    ],
+    [ // $dynamicRoutes
+        35 => [[['_route' => '_preview_error', '_controller' => 'error_controller::preview', '_format' => 'html'], ['code', '_format'], null, null, false, true, null]],
+        54 => [
+            [['_route' => 'find_card', '_controller' => 'App\\Controller\\CardController::find'], ['id'], null, null, false, true, null],
+            [null, null, null, null, false, false, 0],
+        ],
+    ],
+    null, // $checkCondition
+];
